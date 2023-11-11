4a4f885b888230da5b4008f1a47f03b0c3225451
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-11-11 15:35:50 -0500

    Added RGB control keys to 4x6

--- keyboards/handwired/wylderbuilds/trackball/4x6_whole_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/4x6_whole_track/keymaps/vial/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,               KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
         _______,_______,_______,_______,_______,KC_LBRC,               KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
         _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,               KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-        _______,_______,_______,_______,_______,_______,               _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+        _______,RGB_VAI,RGB_SAI,RGB_HUI,RGB_MOD,RGB_TOG,               _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
                                           _______,KC_BTN1,KC_BTN3,     _______,
                                           SNIPING,DRGSCRL,             _______,
                                                                _______,QK_BOOT
