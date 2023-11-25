e273d943949133185aa7839d50a3b907098751d7
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-11-24 19:02:22 -0500

    Adding 5x6_full_wyld_track

---
+++ keyboards/handwired/wylderbuilds/trackball/5x6_full_wyld_track/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAD, 0x0B, 0x8B, 0xB5, 0x65, 0x12, 0xAD, 0x0E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
