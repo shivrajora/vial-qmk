8df265f6d448d9ca88208a801e5cfdc540c4ee78
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-12-15 12:05:43 -0500

    Added 4x6_3_dual_track

---
+++ keyboards/handwired/wylderbuilds/trackball/4x5_3_dual_track/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3A, 0x72, 0x3A, 0x2B, 0x93, 0x77, 0x5E, 0xD7}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
