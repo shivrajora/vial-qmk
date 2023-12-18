c8d669929467e211bbd2a572565acd470782c8e6
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-12-18 11:55:50 -0500

    4x6 skree tail in progress

--- keyboards/handwired/wylderbuilds/trackball/4x6_3_wyld_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/4x6_3_wyld_track/keymaps/vial/keymap.c
@@ -10,52 +10,41 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_4x6(
-        KC_ESC , KC_1, KC_2, KC_3, KC_4, KC_5,                         KC_6, KC_7, KC_8,   KC_9,   KC_0,    KC_MUTE,
-        KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                         KC_Y, KC_U, KC_I,   KC_O,   KC_P,    KC_MINS,
+        KC_ESC,  KC_Q, KC_W, KC_E, KC_R, KC_T,                         KC_Y, KC_U, KC_I,   KC_O,   KC_P,    KC_MINS,
         KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,                         KC_H, KC_J, KC_K,   KC_L,   KC_SCLN, KC_QUOT,
         KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                         KC_N, KC_M, KC_COMM,KC_DOT ,KC_SLSH, DRGSCRL,
-                                 RAISE ,                                    LOWER,
-                                     KC_SPC,                           KC_ENT,
-                                        KC_BSPC,                   KC_DEL
+                       KC_LBRC, KC_RBRC,                                           KC_EQL, KC_RCTL,
+                                RAISE , KC_SPC, KC_BSPC,      KC_TAB, KC_ENT, LOWER
         ),
 
     [_LOWER] = LAYOUT_4x6(
         KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_MUTE,
         _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
         _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-        _______,_______,_______,KC_BTN3,KC_BTN2,KC_BTN1,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,DRGSCRL,
-                                              _______,                               _______,
-                                                  KC_BTN1,                      _______,
-                                                     KC_BTN3,               QK_BOOT
-
+                        DRGSCRL,SNIPING,                                                         KC_P2 , KC_P3,
+                                   _______, KC_BTN1, KC_BTN3,          _______,_______,_______,
         ),
 
     [_RAISE] = LAYOUT_4x6(
         KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6, KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_MUTE ,
         _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NUM,KC_INS ,KC_SCRL,KC_MUTE,
         _______,KC_LEFT,KC_UP,KC_DOWN,KC_RGHT,KC_LPRN,                          KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLD,KC_VOLU,
-        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,DRGSCRL,
-                                              _______,                              _______,
-                                                   _______,                     _______,
-                                                        QK_BOOT,            _______
+                        _______,_______,                                                        _______,_______,
+                                    _______,_______,_______,          _______,_______,_______,
         ),
     [3] = LAYOUT_4x6(
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-                                              _______,                              _______,
-                                                  _______,                     _______,
-                                                       _______,            _______
+                        DRGSCRL,SNIPING,                                                         _______, _______,
+                                    _______, KC_BTN1, KC_BTN3,          _______,_______,_______,
         ),
     [4] = LAYOUT_4x6(
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
-                                              _______,                              _______,
-                                                  _______,                     _______,
-                                                       _______,            _______
+                        _______,_______,                                                        _______,_______,
+                                        _______,_______,_______,          _______,_______,_______,
         )
 };
 
