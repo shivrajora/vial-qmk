acf663bfec23cf70354c1fd8cc5a3ed3e12f8d94
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2024-01-29 15:33:13 -0500

    Updating 4x6_whole_track and 5x7_full_ug finished

--- keyboards/handwired/wylderbuilds/trackball/4x6_whole_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/4x6_whole_track/keymaps/vial/keymap.c
@@ -19,39 +19,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                         KC_Y, KC_U, KC_I,   KC_O,   KC_P,    KC_MINS,
         KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,                         KC_H, KC_J, KC_K,   KC_L,   KC_SCLN, KC_QUOT,
         KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                         KC_N, KC_M, KC_COMM,KC_DOT ,KC_SLSH, KC_BSLS,
-                                         RAISE, KC_SPC, KC_BSPC,       LOWER,
-                                         KC_TAB, KC_HOME,              KC_ENT,
-                                                               KC_DEL, KC_LALT
+                                         RAISE,   KC_SPC,              LOWER,
+                                         KC_BSPC, KC_TAB,          KC_ENT,
+                                         KC_GRV,  KC_HOME,     KC_DEL
+//                                         RAISE, KC_SPC, KC_BSPC,       LOWER,
+//                                         KC_TAB, KC_HOME,              KC_ENT,
+//                                                               KC_DEL, KC_LALT
         ),
 
     [_LOWER] = LAYOUT_4x6(
         KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,               KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
         _______,_______,_______,_______,_______,KC_LBRC,               KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
         _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,               KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-        _______,RGB_VAI,RGB_SAI,RGB_HUI,RGB_MOD,RGB_TOG,               _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-                                          _______,KC_BTN1,KC_BTN3,     _______,
-                                          SNIPING,DRGSCRL,             _______,
-                                                               _______,QK_BOOT
-
+        _______,RGB_VAI,RGB_SAI,RGB_HUI,RGB_MOD,RGB_TOG,               _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,QK_BOOT,
+                                          _______, KC_BTN1,            _______,
+                                          KC_BTN3, KC_BTN2,         _______,
+                                          SNIPING, DRGSCRL,      _______
+//                                          _______,KC_BTN1,KC_BTN3,     _______,
+//                                          SNIPING,DRGSCRL,             _______,
+//                                                               _______,QK_BOOT
         ),
 
     [_RAISE] = LAYOUT_4x6(
         KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,               KC_F6, KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
         _______,_______,_______,_______,_______,KC_LBRC,               KC_RBRC,_______,KC_NUM,KC_INS ,KC_SCRL,KC_MUTE,
-        _______,KC_LEFT,KC_UP,KC_DOWN,KC_RGHT,KC_LPRN,                 KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-        _______,_______,_______,_______,_______,_______,               _______,_______,_______,_______,WYLD_AUTO_MS_TOG,KC_VOLD,
-                                          _______,_______,_______,     _______,
-                                          QK_BOOT,_______,             _______,
-                                                               _______,_______
+        _______,KC_LEFT,KC_UP,KC_DOWN,KC_RGHT,KC_LPRN,                 KC_RPRN,KC_BTN1,KC_BTN3,KC_BTN2,_______,KC_VOLU,
+        QK_BOOT,_______,_______,_______,_______,_______,               _______,_______,_______,_______,WYLD_AUTO_MS_TOG,KC_VOLD,
+                                          _______, _______,            _______,
+                                          _______, _______,         _______,
+                                          _______, _______,      _______
         ),
     [_MOUSE] = LAYOUT_4x6(
         _______,_______,_______,_______,_______,_______,               _______,_______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,               _______,_______,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,               _______,KC_BTN1,KC_BTN3,KC_BTN2,_______,_______,
         _______,_______,_______,_______,_______,_______,               _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,               _______,_______,_______,_______,_______,_______,
-                                          _______,KC_BTN1,KC_BTN3,     _______,
-                                          SNIPING,DRGSCRL,             _______,
-                                                               _______,_______
+                                          _______, KC_BTN1,            _______,
+                                          KC_BTN3, KC_BTN2,         _______,
+                                          SNIPING, DRGSCRL,      _______
+//                                          _______,KC_BTN1,KC_BTN3,     _______,
+//                                          SNIPING,DRGSCRL,             _______,
+//                                                               _______,_______
         ),
     [4] = LAYOUT_4x6(
         _______,_______,_______,_______,_______,_______,               _______,_______,_______,_______,_______,_______,
@@ -64,9 +72,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         )
 };
 
+void keyboard_post_init_user(void) {
+#ifdef CONSOLE_ENABLE
+    debug_enable=true;
+    debug_matrix=true;
+    debug_keyboard=true;
+    debug_mouse=true;
+#else
+    debug_enable=false;
+    debug_matrix=false;
+    debug_keyboard=false;
+    debug_mouse=false;
+#endif
+#ifdef RGBLIGHT_ENABLE
+    rgblight_layers = my_rgb_layers;
+
+//    rgblight_layers = my_rgb_layers;
+//    rgblight_enable_noeeprom(); // Enables RGB, without saving settings
+//    rgblight_sethsv_noeeprom_cyan();
+//    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+#endif
+}
+
 void pointing_device_init_user(void) {
     //    set_auto_mouse_layer(<mouse_layer>); // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
     set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
+    set_auto_mouse_enable(false);        // but then disable so it's off by default
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -229,25 +260,5 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 //    rgblight_enable();
 
-void keyboard_post_init_user(void) {
-#ifdef CONSOLE_ENABLE
-    debug_enable=true;
-    debug_matrix=true;
-    debug_keyboard=true;
-    debug_mouse=true;
-#else
-    debug_enable=false;
-    debug_matrix=false;
-    debug_keyboard=false;
-    debug_mouse=false;
-#endif
-#ifdef RGBLIGHT_ENABLE
-    rgblight_layers = my_rgb_layers;
 
-}
-//    rgblight_layers = my_rgb_layers;
-//    rgblight_enable_noeeprom(); // Enables RGB, without saving settings
-//    rgblight_sethsv_noeeprom_cyan();
-//    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-#endif
 
