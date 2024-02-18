5abeea72ddb9cfad4ce440b8a70807e2230527aa
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2024-02-18 15:15:22 -0500

    First pass at 6x7_wyld_track

---
+++ keyboards/handwired/wylderbuilds/trackball/6x7_wyld_track/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF8, 0x5A, 0x29, 0x2E, 0x79, 0xD9, 0xC9, 0xE8}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 6 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }
