d3b1bd2e847fe035f01e9ff475350ac776fd5907
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2024-05-02 18:50:47 -0400

    Debugging config

--- keyboards/handwired/wylderbuilds/trackball/5x6_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/5x6_track/keymaps/vial/keymap.c
@@ -23,44 +23,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
         KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                      KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
         KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                      KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-        KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                      KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+        KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  , KC_MUTE,    KC_HOME, KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
         KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                      KC_N  , KC_M  , KC_COMM,KC_DOT ,KC_SLSH,KC_BSLS,
                          KC_LBRC,KC_RBRC,                                                     KC_PLUS,KC_EQL,
-                                                RAISE, KC_SPC,                LOWER,
-                                                KC_TAB, KC_HOME,              KC_ENT,
-                                                KC_BSPC, KC_GRV,     KC_LGUI, KC_LALT
+                                          RAISE, KC_SPC, KC_BSPC,        KC_LALT, KC_ENT, LOWER,
+                                                 KC_TAB, KC_HOME
         ),
 
     [_LOWER] = LAYOUT_5x6(
+        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                     KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
+        _______,_______,_______,_______,_______,KC_LBRC,                     KC_RBRC,_______,KC_NUM,KC_INS ,KC_SCRL,KC_MUTE,
+        _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN, _______,   _______, KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLD,KC_VOLU,
+        _______,RGB_VAI,RGB_SAI,RGB_HUI,RGB_MOD,RGB_TOG,                     _______,_______,_______,_______,_______,QK_BOOT,
+                        _______,_______,                                                     KC_RALT ,KC_RCTL,
+                                          _______,_______,_______,    _______,_______,_______,
+                                                  _______,_______
+        ),
+    [_RAISE] = LAYOUT_5x6(
         KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                     KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
         WYLD_AUTO_MS_TOG,_______,_______,_______,_______,KC_LBRC,            KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,QK_BOOT,KC_PLUS,
-        _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                     KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-        DPI_RMOD,DPI_MOD,DPI_RST,_______,_______,KC_PSCR,                    _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-                         SNIPING,DRGSCRL,                                                     KC_INS, KC_P0,
-                                                _______, KC_BTN1,            _______,
-                                                KC_BTN3, KC_BTN2,            _______,
-                                                SNIPING,DRGSCRL,     _______,QK_BOOT
+        _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN, _______,   _______, KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
+        QK_BOOT,DPI_RMOD,DPI_MOD,DPI_RST,SNIPING,DRGSCRL,                    _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+                         _______,_______,                                                     KC_INS, KC_P0,
+                                          _______,KC_BTN1,KC_BTN2,    _______,_______,_______,
+                                                  KC_BTN3,_______
         ),
 
-    [_RAISE] = LAYOUT_5x6(
-        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                     KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-        _______,QK_BOOT,_______,_______,_______,KC_LBRC,                     KC_RBRC,_______,KC_NUM,KC_INS ,KC_SCRL,KC_MUTE,
-        _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                     KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-        _______,RGB_VAI,RGB_SAI,RGB_HUI,RGB_MOD,RGB_TOG,                     _______,_______,_______,_______,_______,KC_VOLD,
-                        _______,_______,                                                     KC_RALT ,KC_RCTL,
-                                                _______,_______,             _______,
-                                                _______,_______,             _______,
-                                                QK_BOOT,_______,     _______,_______
-        ),
     [_MOUSE] = LAYOUT_5x6(
         _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,                    _______,KC_BTN1,KC_BTN3,KC_BTN2,_______,_______,
-        _______,_______,_______,_______,_______,_______,                    _______,_______,_______,_______,_______,_______,
-                        SNIPING,DRGSCRL,                                                    _______ ,_______,
-                                               _______, KC_BTN1,           _______,
-                                                KC_BTN3, KC_BTN2,           _______,
-                                                SNIPING,DRGSCRL,    _______,QK_BOOT
+        _______,_______,_______,_______,_______,_______, _______,  _______, _______,KC_BTN1,KC_BTN3,KC_BTN2,_______,_______,
+        _______,_______,_______,_______,SNIPING,DRGSCRL,                    _______,_______,_______,_______,_______,_______,
+                        _______,_______,                                                    _______ ,_______,
+                                          _______,KC_BTN1,KC_BTN2,    _______,_______,_______,
+                                                  KC_BTN3,_______
         )
 };
 
@@ -218,16 +214,16 @@ bool oled_task_user(void) {
 
     if (is_keyboard_left()) {
         //        oled_write_P(PSTR("Layer\n"), false);
-        oled_set_cursor(0, 2);
+        oled_set_cursor(0, 3);
         oled_write_P(PSTR(layer_name), false);
 
-        oled_set_cursor(2, 4);
-        oled_write_P(PSTR("WPM "), false);
         oled_set_cursor(2, 6);
+        oled_write_P(PSTR("WPM "), false);
+        oled_set_cursor(2, 7);
         oled_write(get_u8_str(get_current_wpm(), ' '), false);
         // Host Keyboard LED Status
 
-        oled_set_cursor(0, 8);
+        oled_set_cursor(0, 9);
         led_t led_state = host_keyboard_led_state();
 
         oled_write_P(led_state.num_lock ? PSTR("NUMLK \n") : PSTR("    \n"), false);
@@ -238,31 +234,23 @@ bool oled_task_user(void) {
 #ifdef POINTING_DEVICE_ENABLE
         char* mode = get_mouse_mode_string();
         uint16_t dpi = get_current_dpi();
-        oled_set_cursor(2, 2);
+        oled_set_cursor(2, 3);
         oled_write_P(PSTR("DPI "), false);
-        oled_set_cursor(0, 4);
+        oled_set_cursor(0, 5);
         oled_write_P(PSTR(get_u16_str(dpi, ' ')), false);
-        oled_set_cursor(0, 7);
-        oled_write(PSTR(mode), false);
-#ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
         oled_set_cursor(0, 9);
-        if (get_auto_mouse_enable()) {
-            oled_write(PSTR("Auto"), false);
-        } else {
-            oled_write(PSTR("     "), false);
-        }
-
-#endif // POINTING_DEVICE_AUTO_MOUSE_ENABLE
+        oled_write(PSTR(mode), false);
 #else
         oled_set_cursor(0, 0);
         render_wylderbuilds();
         oled_scroll_left();
-#endif  // POINTING_DEVICE_ENABLE
+#endif
     }
 
     return false;
 }
-#endif // OLED_ENABLE
+#endif
+
 
 #ifdef RGBLIGHT_ENABLE
 const rgblight_segment_t PROGMEM querty_layer[] = RGBLIGHT_LAYER_SEGMENTS(
