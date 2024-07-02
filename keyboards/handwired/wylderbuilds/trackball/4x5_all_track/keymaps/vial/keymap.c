4412bd6ce482eaf89074c35e533ad538d4eaa18b
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2024-07-02 16:12:20 -0400

    Refuses to compile on mac...?

--- keyboards/handwired/wylderbuilds/trackball/4x5_all_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/4x5_all_track/keymaps/vial/keymap.c
@@ -16,10 +16,10 @@ enum wyld_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_4x5(
-            KC_1, KC_2, KC_3, KC_4, KC_5,                         KC_6, KC_7, KC_8,   KC_9,   KC_0,
             KC_Q, KC_W, KC_E, KC_R, KC_T,                         KC_Y, KC_U, KC_I,   KC_O,   KC_P,
             KC_A, KC_S, KC_D, KC_F, KC_G,                         KC_H, KC_J, KC_K,   KC_L,   KC_SCLN,
             KC_Z, KC_X, KC_C, KC_V, KC_B,                         KC_N, KC_M, KC_COMM,KC_DOT ,KC_SLSH,
+                        KC_LBRC, KC_RBRC,                                     KC_PLUS, KC_EQL,
                                       RAISE, KC_SPC,               LOWER,
                                       KC_TAB, KC_BSPC,             KC_ENT,
                                       KC_DEL, KC_GRV,     KC_HOME, KC_LALT
@@ -30,6 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                  KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,
             _______, _______,_______, _______, _______,               KC_P0,   KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,
             KC_LPRN, KC_RPRN,_______,SNIPING, DRGSCRL,                _______, KC_LEFT, KC_DOWN, KC_UP,KC_RGHT,
+                     _______,_______,                                                   _______,_______,
                                         _______,_______,             _______,
                                         KC_BTN3,KC_BTN1,             _______,
                                         SNIPING,DRGSCRL,     _______,QK_BOOT
@@ -40,6 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             DPI_RMOD,DPI_MOD,DPI_RST,_______,KC_LBRC,                    KC_RBRC,_______,KC_NUM,KC_INS ,KC_SCRL,
             KC_LEFT,KC_UP,KC_DOWN,KC_RGHT,KC_LPRN,                      KC_RPRN,KC_MPRV,KC_MPLY,KC_VOLU,KC_VOLD,
             WYLD_AUTO_MS_TOG,_______,_______,_______,_______,           _______,_______,_______,_______,_______,
+                             _______,_______,                                           _______,_______,
                                         _______,_______,             _______,
                                         _______,_______,             _______,
                                         QK_BOOT,_______,     _______,_______
@@ -49,6 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,
         _______,KC_BTN2,KC_BTN3,KC_BTN1,_______,                        _______,KC_BTN1,KC_BTN3,KC_BTN2,_______,
         _______,_______,_______,SNIPING, DRGSCRL,                       _______,_______,_______,_______,_______,
+                _______,_______,                                                        _______,_______,
                                         _______,_______,             _______,
                                         KC_BTN3,KC_BTN1,             _______,
                                         SNIPING,DRGSCRL,     _______,_______
