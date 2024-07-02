a7f8e2adcdae6ff5d50ef46d9d57cce3ae3451c0
andy <nethermead@gmail.com>
2024-07-02 18:11:27 -0400

    4x5_all_track fix

--- keyboards/handwired/wylderbuilds/trackball/4x5_all_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/4x5_all_track/keymaps/vial/keymap.c
@@ -29,8 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______,_______,_______,_______,KC_LBRC,                  KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,KC_PLUS,
             KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                  KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,
             _______, _______,_______, _______, _______,               KC_P0,   KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,
-            KC_LPRN, KC_RPRN,_______,SNIPING, DRGSCRL,                _______, KC_LEFT, KC_DOWN, KC_UP,KC_RGHT,
-                     _______,_______,                                                   _______,_______,
+                     SNIPING, DRGSCRL,                                                   _______,_______,
                                         _______,_______,             _______,
                                         KC_BTN3,KC_BTN1,             _______,
                                         SNIPING,DRGSCRL,     _______,QK_BOOT
@@ -40,8 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                     KC_F6, KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,
             DPI_RMOD,DPI_MOD,DPI_RST,_______,KC_LBRC,                    KC_RBRC,_______,KC_NUM,KC_INS ,KC_SCRL,
             KC_LEFT,KC_UP,KC_DOWN,KC_RGHT,KC_LPRN,                      KC_RPRN,KC_MPRV,KC_MPLY,KC_VOLU,KC_VOLD,
-            WYLD_AUTO_MS_TOG,_______,_______,_______,_______,           _______,_______,_______,_______,_______,
-                             _______,_______,                                           _______,_______,
+                             WYLD_AUTO_MS_TOG,_______,                                 _______,_______,
                                         _______,_______,             _______,
                                         _______,_______,             _______,
                                         QK_BOOT,_______,     _______,_______
@@ -50,8 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,
         _______,KC_BTN2,KC_BTN3,KC_BTN1,_______,                        _______,KC_BTN1,KC_BTN3,KC_BTN2,_______,
-        _______,_______,_______,SNIPING, DRGSCRL,                       _______,_______,_______,_______,_______,
-                _______,_______,                                                        _______,_______,
+                SNIPING, DRGSCRL,                                                        _______,_______,
                                         _______,_______,             _______,
                                         KC_BTN3,KC_BTN1,             _______,
                                         SNIPING,DRGSCRL,     _______,_______
