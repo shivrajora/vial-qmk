9a3b749bac56d3c81048d51aedded47a2a8f7ae4
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2024-07-05 18:35:23 -0400

    Reworked keymap a bit for 3x5

--- keyboards/handwired/wylderbuilds/trackball/4x5_all_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/4x5_all_track/keymaps/vial/keymap.c
@@ -22,33 +22,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         KC_LBRC, KC_RBRC,                                     KC_PLUS, KC_EQL,
                                       RAISE, KC_SPC,               LOWER,
                                       KC_TAB, KC_BSPC,             KC_ENT,
-                                      KC_DEL, KC_GRV,     KC_HOME, KC_LALT
+                                      KC_DEL, KC_RSFT,    KC_LSFT, KC_LALT
     ),
 
     [_LOWER] = LAYOUT_4x5(
-            _______,_______,_______,_______,KC_LBRC,                  KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,KC_PLUS,
-            KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                  KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,
-            _______, _______,_______, _______, _______,               KC_P0,   KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,
-                     SNIPING, DRGSCRL,                                                   _______,_______,
+            KC_1,   KC_2,   KC_3,    KC_4,    KC_5,                    KC_6,           KC_7,   KC_8,   KC_9,  KC_0,
+            KC_HOME,KC_PGUP,_______, KC_LPRN ,KC_RPRN,                 KC_KP_SLASH,    KC_P4 , KC_P5 , KC_P6 ,KC_PLUS,
+            KC_END, KC_PGDN,_______, KC_LBRC, KC_RBRC,                 KC_KP_ENTER,    KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,
+                     KC_GRV,  _______,                                                         KC_P0, KC_KP_DOT,
                                         _______,_______,             _______,
                                         KC_BTN3,KC_BTN1,             _______,
                                         SNIPING,DRGSCRL,     _______,QK_BOOT
     ),
 
     [_RAISE] = LAYOUT_4x5(
-            KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                     KC_F6, KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,
+            KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                      KC_F6, KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,
             DPI_RMOD,DPI_MOD,DPI_RST,_______,KC_LBRC,                    KC_RBRC,_______,KC_NUM,KC_INS ,KC_SCRL,
-            KC_LEFT,KC_UP,KC_DOWN,KC_RGHT,KC_LPRN,                      KC_RPRN,KC_MPRV,KC_MPLY,KC_VOLU,KC_VOLD,
-                             WYLD_AUTO_MS_TOG,_______,                                 _______,_______,
+            KC_LEFT,KC_UP,KC_DOWN,KC_RGHT, KC_LPRN,                      KC_RPRN,KC_MPRV,KC_MPLY,KC_VOLU,KC_VOLD,
+                          WYLD_AUTO_MS_TOG,_______,                                      _______,_______,
                                         _______,_______,             _______,
                                         _______,_______,             _______,
                                         QK_BOOT,_______,     _______,_______
     ),
     [_MOUSE] = LAYOUT_4x5(
         _______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,
         _______,KC_BTN2,KC_BTN3,KC_BTN1,_______,                        _______,KC_BTN1,KC_BTN3,KC_BTN2,_______,
-                SNIPING, DRGSCRL,                                                        _______,_______,
+        _______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,
+                SNIPING, DRGSCRL,                                                       _______,_______,
                                         _______,_______,             _______,
                                         KC_BTN3,KC_BTN1,             _______,
                                         SNIPING,DRGSCRL,     _______,_______
