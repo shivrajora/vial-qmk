529d38faf25ad96cc6504383fef76977a5285849
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2024-07-18 16:35:45 -0400

    3_5_mini_3_track finalizing

--- keyboards/handwired/wylderbuilds/trackball/3x5_mini_3_track/3x5_mini_3_track.h
+++ keyboards/handwired/wylderbuilds/trackball/3x5_mini_3_track/3x5_mini_3_track.h
@@ -44,7 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // TRACKBALL IN WALL SETUP
 #define LAYOUT_split_3x5_3( \
     L00, L01, L02, L03, L04,             R00, R01, R02, R03, R04, \
-    L10, L11, L12, L13, L14, L35,   R33, R10, R11, R12, R13, R14, \
+    L10, L11, L12, L13, L14, L35,   R35, R10, R11, R12, R13, R14, \
     L20, L21, L22, L23, L24,             R20, R21, R22, R23, R24, \
                 L32, L33, L34,           R30, R31, R32,           \
                      L30, L31  \
@@ -56,7 +56,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { R00, R01, R02, R03, R04, XXX }, \
     { R10, R11, R12, R13, R14, XXX }, \
     { R20, R21, R22, R23, R24, XXX }, \
-    { R30, R31, R32, R33, XXX, XXX } \
+    { R30, R31, R32, XXX, XXX, R35 } \
 }
 
 
--- keyboards/handwired/wylderbuilds/trackball/3x5_mini_3_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/3x5_mini_3_track/keymaps/vial/keymap.c
@@ -19,8 +19,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_MUTE,      KC_HOME, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                             RAISE,   KC_SPC,  KC_TAB,               KC_HOME, KC_ENT, LOWER,
-                                      KC_LALT, KC_GRV
+                             RAISE,   KC_SPC,  KC_BSPC,               KC_LCTL, KC_ENT, LOWER,
+                                      KC_LALT, KC_TAB
         ),
 
     [_LOWER] = LAYOUT_split_3x5_3(
