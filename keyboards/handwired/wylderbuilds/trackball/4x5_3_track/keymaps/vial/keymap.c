fec57507e1429c4f423cb50c1a5f1e7925f7323b
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2024-11-13 08:12:15 -0500

    Final 4x5_3_track

--- keyboards/handwired/wylderbuilds/trackball/4x5_3_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/4x5_3_track/keymaps/vial/keymap.c
@@ -14,34 +14,34 @@ enum wyld_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_4x5(
+    [_QWERTY] = LAYOUT_4x5_3(
+        KC_1, KC_2, KC_3, KC_4, KC_5,                         KC_6, KC_7, KC_8,   KC_9,   KC_0,
         KC_Q, KC_W, KC_E, KC_R, KC_T,                         KC_Y, KC_U, KC_I,   KC_O,   KC_P,
         KC_A, KC_S, KC_D, KC_F, KC_G,                         KC_H, KC_J, KC_K,   KC_L,   KC_SCLN,
         KC_Z, KC_X, KC_C, KC_V, KC_B,                         KC_N, KC_M, KC_COMM,KC_DOT ,KC_SLSH,
-              KC_LBRC, KC_RBRC,                                           KC_EQL, KC_BSLS,
                          RAISE, KC_SPC, KC_BSPC,      KC_DEL, KC_ENT, LOWER
         ),
 
-    [_LOWER] = LAYOUT_4x5(
+    [_LOWER] = LAYOUT_4x5_3(
+        KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                  KC_CIRC, KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,
         QK_BOOT,_______,_______,_______,KC_LBRC,                  KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,WYLD_AUTO_MS_TOG,
         KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                  KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,
-        _______,_______,_______,SNIPING,DRGSCRL,                  _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,
-                         KC_LBRC, KC_RBRC,                                         KC_EQL, KC_BSLS,
+        DPI_RMOD,DPI_MOD,DPI_RST,SNIPING,DRGSCRL,                 _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,
                             _______, KC_BTN1, KC_BTN3,    _______, _______, _______
         ),
 
-    [_RAISE] = LAYOUT_4x5(
+    [_RAISE] = LAYOUT_4x5_3(
         KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                     KC_F6, KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,
+        _______,_______,_______,_______,KC_LBRC,                    KC_RBRC,KC_NUM,KC_INS ,KC_SCRL,KC_MUTE,
         _______,_______,_______,_______,KC_LBRC,                    KC_RBRC,_______,KC_NUM,KC_INS ,KC_SCRL,
         KC_LEFT,KC_UP,KC_DOWN,KC_RGHT,KC_LPRN,                      KC_RPRN,KC_MPRV,KC_MPLY,KC_VOLU,KC_VOLD,
-                KC_LBRC, KC_RBRC,                                                   KC_EQL, KC_BSLS,
-                                _______, _______,_______,q     _______,_______,_______
+                                _______, _______,_______,     _______,_______,_______
         ),
-    [_MOUSE] = LAYOUT_4x5(
+    [_MOUSE] = LAYOUT_4x5_3(
         _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,
+        _______, _______, KC_BTN2, KC_BTN3, KC_BTN1,         KC_BTN1, KC_BTN3, KC_BTN2, _______, _______,
         _______, _______, _______, SNIPING, DRGSCRL,         _______, _______, _______, _______, _______,
-                 _______, _______,                                             _______, _______,
                             _______, KC_BTN1, KC_BTN3,    _______, _______, _______
         )
 };
@@ -49,6 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void pointing_device_init_user(void) {
 #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
     set_auto_mouse_enable(true);
+    set_auto_mouse_enable(false);
 #endif
 #ifdef POINTING_DEVICE_COMBINED
     pointing_device_set_cpi_on_side(true, CHARYBDIS_DRAGSCROLL_DPI); // LEFT
